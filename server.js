f4d48edbb7e5ce6f2e29e3b35651bac6e76d7ca9
fo95116 <fo95116@gmail.com>
2016-08-11 19:22:19 -0700

    server APi requests finished

--- server.js
+++ server.js
@@ -40,6 +40,13 @@ function handleError(res, reason, message, code) {
 }
 
 app.get("/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
 
 app.post("/contacts", function(req, res) {
@@ -66,10 +73,29 @@ app.post("/contacts", function(req, res) {
  */
 
  app.get("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne({_id: new ObjectID(req.params.id)}, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contact");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
  });
 
  app.put("/contacts/:id", function(req, res) {
+  if (err) {
+    handleError(res, err.message, "Failed to update contact");
+  } else {
+    res.status(204).end();
+  }
  });
 
  app.delete("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.massage, "Failed to delete contact");
+    } else {
+      res.status(204).end();
+    }
+  });
  });
